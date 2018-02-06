ca7fc65297c46e0e5b6c6ea8ff2c1e6b608e365a
J-MS <35871481+J-MS@users.noreply.github.com>
2018-02-06 14:24:17 -0600

    This improves the finding of primes and their size

--- example.py
+++ example.py
@@ -1,13 +1,25 @@
 from random import randint;
+from math import sqrt;
+from math import ceil;
 
 #This is intentionally horrible, albeit working, code. Alter it to make it
 #more readable, styled, and documented. Also, why 500? Make this a more
 #realistic and 'smart' number. How big should I primes be, should they really
 #start at two?
-primes = [p for p in range(2, 500) if 0 not in [p % c for c in range(2, p)]];
-
+primes = [2];
+for interger in range(3, 100000): 
+	root = ceil(sqrt(interger))
+	for index, prime in enumerate(primes):
+		if prime >= root: #checks if we are geting too big
+			break
+		elif interger % prime == 0:
+			break
+		else:
+			primes.append(interger)
 #This should also be changed.
 sharedPrime = primes[randint(0, len(primes) - 1)];
+while sharedPrime < 1000:
+	sharedPrime = primes[randint(0, len(primes) - 1)];
 sharedBase = randint(5, 1000000);
 
 #Alice and Bob's secret Keys.
@@ -37,3 +49,5 @@ print("Alice Shared Secret Key: " + str(aliceSharedSK));
 #For Bob
 bobSharedSK = ((A ** bobSK) % sharedPrime);
 print("Bob Shared Secret Key: " + str(bobSharedSK));
+
+# next thing to do is a way to encrypt a message, and decrypt
